41f4ff9baedea78631bee6d630acf3c75c601e37
dev <devsaini150602@gmail.com>
2024-08-28 15:52:24 +0530

    commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app=express();
 const port=process.env.PORT;
 
 app.use(cors({
-    origin:'https://real-time-chat-app-qafy.onrender.com',
+    origin:[process.env.ORIGIN],
     methods:["GET","POST","PUT","PATCH","DELETE"],
     credentials:true,
 }))
