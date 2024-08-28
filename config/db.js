3f513630139850002568a563f449c0f2ab96070c
dev <devsaini150602@gmail.com>
2024-08-28 15:22:28 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -6,7 +6,10 @@ dotenv.config();
 const dbconnect=()=>{
     const db_url=process.env.DATABASE_URL;
     
-    mongoose.connect(db_url,{})
+    mongoose.connect(db_url,{
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    })
     .then(()=>{
         console.log('db connection is successful')
     })
