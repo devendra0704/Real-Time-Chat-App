7b412a3f7b20f76c10d01716bb0d92e2109f543e
dev <devsaini150602@gmail.com>
2024-08-29 00:26:59 +0530

    commit

--- client/src/lib/api-client.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-import { HOST } from "@/utils/constant"
-const apiClient= axios.create({
-    baseURL:HOST,
-})
-
-export default apiClient;
--- client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createAuthSlice } from "./slices/authSlice";
-import {create} from "zustand"
-import { createChatSlice } from "./slices/chatSlice";
-
-export const useAppStore= create()((...a)=>({
-    ...createAuthSlice(...a),
-    ...createChatSlice(...a)
-}))
