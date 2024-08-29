f3152032454df832c6fec86c05854667f56e5ac8
dev <devsaini150602@gmail.com>
2024-08-29 14:08:54 +0530

    commit

--- client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -111,42 +111,55 @@ const MessageBar = () => {
   }
 
   return (
-    <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6">
-      <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-5">
+    <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-4 sm:px-8 mb-12  md:mb-6 sm:mb-6 gap-1 sm:gap-6">
+      <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-3 sm:gap-5 pr-2 sm:pr-5">
         <input
           type="text"
-          className="flex-1 p-5 bg-transparent rounded-md focus:border-none focus:outline-none"
+          className="flex-1 p-3 sm:p-5 bg-transparent rounded-md focus:border-none focus:outline-none"
           placeholder="Enter message"
           value={message}
-          onChange={(e)=>setMessage(e.target.value)}
+          onChange={(e) => setMessage(e.target.value)}
         />
-        <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all" onClick={handleAttachmentClick}>
-        <GrAttachment className="text-2xl"/>
+        <button
+          className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
+          onClick={handleAttachmentClick}
+        >
+          <GrAttachment className="text-xl sm:text-2xl" />
         </button>
 
-        <input type="file" className="hidden" ref={fileInputRef} onChange={handleAttachmentChange} />
+        <input
+          type="file"
+          className="hidden"
+          ref={fileInputRef}
+          onChange={handleAttachmentChange}
+        />
 
         <div className="relative">
-          <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all" onClick={handleSetEmoji} >
-            <RiEmojiStickerLine className="text-2xl"/>
+          <button
+            className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
+            onClick={handleSetEmoji}
+          >
+            <RiEmojiStickerLine className="text-xl sm:text-2xl" />
           </button>
-          
-          <div className="absolute bottom-12 right-0" ref={emojiRef}>
-            <EmojiPicker 
+
+          <div className="absolute bottom-12 right-[-70px] sm:right-0" ref={emojiRef}>
+            <EmojiPicker
               theme="dark"
               open={emojiPickerOpen}
               onEmojiClick={handleAddEmoji}
               autoFocusSearch={false}
             />
           </div>
-
         </div>
       </div>
-        <button className="bg-[#8417ff] rounded-md flex items-center justify-center p-5 focus:border-none hover:bg-[#741bda] focus:bg-[#741bda]  focus:outline-none focus:text-white duration-300 transition-all " onClick={handleSendMessage}>
-              <IoSend className="text-2xl"/>
-        </button>
+      <button
+        className="bg-[#8417ff] rounded-md flex items-center justify-center p-3 sm:p-5 focus:border-none hover:bg-[#741bda] focus:bg-[#741bda] focus:outline-none focus:text-white duration-300 transition-all"
+        onClick={handleSendMessage}
+      >
+        <IoSend className="text-xl sm:text-2xl" />
+      </button>
     </div>
-  )
+  );
 }
 
 export default MessageBar;
